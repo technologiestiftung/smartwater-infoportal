fa830b4547c25385e2afc5ba34a71ea6b4db2caa
Jonathan <jonathan.haudenschild@ts.berlin>
2025-04-29 16:34:55 +0200

    feat: added text and adjust i18n

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -5,8 +5,18 @@ export default getRequestConfig(async () => {
 	// read from `cookies()`, `headers()`, etc.
 	const locale = "de";
 
+	const messages = {
+		...(await import(`../../messages/${locale}/home.json`)).default,
+		...(await import(`../../messages/${locale}/common.json`)).default,
+		...(await import(`../../messages/${locale}/checkResults.json`)).default,
+		...(await import(`../../messages/${locale}/evaluation.json`)).default,
+		...(await import(`../../messages/${locale}/generalInfo.json`)).default,
+		...(await import(`../../messages/${locale}/questionnaire.json`)).default,
+		...(await import(`../../messages/${locale}/recommendations.json`)).default,
+	};
+
 	return {
 		locale,
-		messages: (await import(`../../messages/${locale}.json`)).default,
+		messages,
 	};
 });
