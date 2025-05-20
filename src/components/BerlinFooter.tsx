c9aa82cd71685b9d9f933db6f404f3089ebee914
Jakob Sawal <jakob.sawal@ts.berlin>
2025-05-20 14:55:03 +0200

    feat: remove any actual api request in BerlinFooter component for testing

--- src/components/BerlinFooter.tsx
+++ src/components/BerlinFooter.tsx
@@ -1,29 +1,15 @@
-import { formatGermanTimestamp } from "@/lib/utils";
 import { useEffect, useState } from "react";
 
 export default function BerlinFooter() {
 	const [html, setHtml] = useState("");
-	const [timestamp, setTimestamp] = useState<number | null>(null);
 
 	useEffect(() => {
-		fetch("/api/berlin-footer")
-			.then((res) => res.json())
-			.then((data) => {
-				setHtml(data.html);
-				setTimestamp(data.timestamp);
-			});
+		setHtml("<h1>Test Berlin Footer...</h1>");
 	}, []);
 
 	return (
 		<div className="mx-auto flex w-full flex-grow flex-col py-5">
-			{" "}
-			{/* md:max-w-[61.25rem] */}
 			<div dangerouslySetInnerHTML={{ __html: html }} />
-			{timestamp && (
-				<p className="mt-4 text-sm text-gray-600">
-					Footer zuletzt aktualisiert: {formatGermanTimestamp(timestamp)}
-				</p>
-			)}
 		</div>
 	);
 }
