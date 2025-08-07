af83083b91614c1632ac9f251dd960ffe85f2374
Jonathan Haudenschild <jonathan.haudenschild@ts.berlin>
2025-08-07 16:42:29 +0200

    fix: turn off css

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	optimizeCss: false,
 };
 
 const withNextIntl = createNextIntlPlugin();
