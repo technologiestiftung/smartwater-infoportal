0af33e200c4fc841a0c80099bb940d99d0752331
Jakob Sawal <jakob.sawal@ts.berlin>
2025-11-05 14:12:21 +0100

    fix: finalize error state

--- src/components/Warning.tsx
+++ src/components/Warning.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { Button, Image } from "berlin-ui-library";
 import { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
+import { cn } from "@/lib/utils";
 
 type WarningProps = {
 	warning: boolean;
@@ -30,23 +31,33 @@ export default function Warning() {
 		requestWarning();
 	}, []);
 
-	const background = warning?.warning
-		? "bg-message-error"
-		: "bg-message-no-warning";
+	const getBackground = () => {
+		if (warning?.warning) {
+			return "bg-message-error";
+		} else if (warning?.error) {
+			return "bg-message-warning-error";
+		}
+		return "bg-message-no-warning";
+	};
+
+	const iconSrc = (() => {
+		if (warning?.warning) {
+			return "/warning.svg";
+		}
+		if (warning?.error) {
+			return "/error.svg";
+		}
+		return "/no-warning.svg";
+	})();
 
 	return (
 		<div
-			className={`overflow-hidden ${background} flex w-full items-center justify-center gap-6 p-5`}
+			className={cn(
+				"flex w-full items-center justify-center gap-6 overflow-hidden p-5",
+				getBackground(),
+			)}
 		>
-			<Image
-				className="w-16"
-				src={
-					warning?.warning || warning?.error
-						? "/warning.svg"
-						: "/no-warning.svg"
-				}
-				alt="Warning Icon"
-			/>
+			<Image className="w-16" src={iconSrc} alt="Warning Icon" />
 			<div>
 				<p>
 					{(() => {
@@ -58,17 +69,24 @@ export default function Warning() {
 						return t("common.warning.noWarning");
 					})()}
 				</p>
-				<Link
-					href="https://wasserportal.berlin.de/warnungen.php"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Button variant="linkWithIcon">
-						{t("common.warning.furtherInformation")}
-					</Button>
-				</Link>
+				{!warning?.error && (
+					<Link
+						href="https://wasserportal.berlin.de/warnungen.php"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Button variant="linkWithIcon">
+							{t("common.warning.furtherInformation")}
+						</Button>
+					</Link>
+				)}
 				{warning?.timeStamp && (
-					<p className="copyright text-grey-darkest">
+					<p
+						className={cn(
+							"copyright text-grey-darkest",
+							warning?.error && "mt-2",
+						)}
+					>
 						{t("common.warning.timeStamp", {
 							timeStamp: warning?.timeStamp,
 						})}
