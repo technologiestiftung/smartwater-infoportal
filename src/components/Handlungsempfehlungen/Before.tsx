d587e599d32d78e219a1d2f56544b1226c24e5c6
Jakob Sawal <jakob.sawal@ts.berlin>
2025-11-06 16:27:07 +0100

    feat: new content

--- src/components/Handlungsempfehlungen/Before.tsx
+++ src/components/Handlungsempfehlungen/Before.tsx
@@ -1,295 +1,267 @@
-import React from "react";
-import { useTranslations } from "next-intl";
-import TextBlock from "../TextBlock";
-import { Button, Image } from "berlin-ui-library";
+import React, { ReactNode } from "react";
+import { useMessages, useTranslations } from "next-intl";
 import Link from "next/link";
+import NextImage from "next/image";
+import { Image } from "berlin-ui-library";
+import { cn } from "@/lib/utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+
+type TocMap = Record<string, string>;
+const lisztIconSize = 32;
 
 const Before: React.FC = () => {
-	const t = useTranslations();
+	const t = useTranslations("recommendations.beforeEvent");
+	const content = useMessages() as {
+		recommendations: {
+			beforeEvent: {
+				list1: TocMap;
+				list2: TocMap;
+				list3: TocMap;
+				list4: TocMap;
+				list5: TocMap;
+			};
+		};
+	};
+	const list1 = content.recommendations.beforeEvent.list1;
+	const list2 = content.recommendations.beforeEvent.list2;
+	const list3 = content.recommendations.beforeEvent.list3;
+	const list4 = content.recommendations.beforeEvent.list4;
+	const list5 = content.recommendations.beforeEvent.list5;
+
+	const getLink = (key: string, chunks: ReactNode, listKey: string) => {
+		let text = "";
+		if (typeof chunks === "string") {
+			text = chunks;
+		} else if (Array.isArray(chunks)) {
+			text = chunks.join("");
+		}
+
+		if (key === "entry1") {
+			if (text.includes("länderübergreifende")) {
+				return {
+					target: "_blank",
+					link: "https://www.hochwasserzentralen.de/",
+				};
+			}
+			return {
+				target: "_self",
+				link: "/#hochwasser-check",
+			};
+		}
+		if (key === "entry2") {
+			if (listKey === "list1") {
+				return {
+					target: "_blank",
+					link: text.includes("risiko")
+						? "https://www.berlin.de/sen/uvk/umwelt/wasser-und-geologie/hochwasser/hochwasserrisikomanagementrichtlinie/#risikokarten"
+						: "https://www.berlin.de/sen/uvk/umwelt/wasser-und-geologie/starkregen-und-ueberflutungen/gefahren/",
+				};
+			}
+			return {
+				target: "_blank",
+				link: "https://www.gdv.de/gdv/statistik/datenservice-zum-naturgefahrenreport/sachversicherung-elementar",
+			};
+		}
+		if (key === "entry4") {
+			return {
+				target: "_blank",
+				link: "#",
+			};
+		}
+		if (key === "entry5") {
+			return {
+				target: "_blank",
+				link: text.includes("NINA")
+					? "https://www.bbk.bund.de/DE/Warnung-Vorsorge/Warn-App-NINA/warn-app-nina_node.html"
+					: "https://www.dwd.de/DE/service/dwd-apps/dwdapps_node.html",
+			};
+		}
+		if (key === "entry6") {
+			return {
+				target: "_blank",
+				link: "https://www.dwd.de/DE/wetter/warnungen_aktuell/kriterien/warnstufen.html",
+			};
+		}
+		if (key === "entry7") {
+			return {
+				target: "_blank",
+				link: "https://www.berlin.de/katastrophenschutz/warnung-und-information/sirenen/artikel.1578804.php#headline_1_39",
+			};
+		}
+		if (key === "entry8") {
+			return {
+				target: "_blank",
+				link: "https://www.berlin.de/katastrophenschutz/warnung-und-information/anlaufstellen-fuer-die-bevoelkerung/",
+			};
+		}
+		if (key === "entry9") {
+			return {
+				target: "_blank",
+				link: "https://www.bbk.bund.de/SharedDocs/Downloads/DE/Mediathek/Publikationen/Buergerinformationen/Ratgeber/ratgeber-notfallvosorge-checkliste.pdf?__blob=publicationFile",
+			};
+		}
+		if (key === "entry10") {
+			return {
+				target: "_blank",
+				link: "https://www.bbk.bund.de/SharedDocs/Downloads/DE/Mediathek/Publikationen/Buergerinformationen/Ratgeber/ratgeber-notfallvosorge-checkliste.pdf?__blob=publicationFile",
+			};
+		}
+		if (key === "entry11") {
+			return {
+				target: "_blank",
+				link: "https://www.bbk.bund.de/SharedDocs/Downloads/DE/Mediathek/Publikationen/Buergerinformationen/Ratgeber/ratgeber-notfallvosorge-checkliste.pdf?__blob=publicationFile",
+			};
+		}
+		return {
+			target: "_self",
+			link: "#",
+		};
+	};
+
+	const Lists = [
+		{
+			listKey: "list1",
+			img: "/HandlungsempfehlungIcons/icon_verhaltensvorsorge.png",
+			list: list1,
+		},
+		{
+			listKey: "list2",
+			img: "/HandlungsempfehlungIcons/icon_versicherung.png",
+			list: list2,
+			fullIMG: "/BeforeList2IMG.jpg",
+		},
+		{
+			listKey: "list3",
+			img: "/HandlungsempfehlungIcons/icon_Trockenvorsorge.png",
+			list: list3,
+			afterList: "afterList3",
+		},
+		{
+			listKey: "list4",
+			img: "/HandlungsempfehlungIcons/icon_Nassvorsorge.png",
+			list: list4,
+		},
+		{
+			listKey: "list5",
+			img: "/HandlungsempfehlungIcons/icon_Ausweichen.png",
+			list: list5,
+			afterList: "afterList5",
+			afterListImage: "/Hochwasserschutzfibel_Bild.png",
+		},
+	];
+
 	return (
-		<div className="flex flex-col gap-12">
-			<section className="">
-				<TextBlock
-					desktopColSpans={{ col1: 2, col2: 3 }}
-					className="w-full gap-6"
-					reverseDesktopColumns={true}
-					slotA={
-						<div className="flex w-full flex-col gap-6">
-							<h3 className="">
-								{t("recommendations.beforeEvent.personalPreparedness.title1")}
+		<section className="mb-12 flex flex-col gap-12">
+			<h2 className="font-normal">
+				{t.rich("intro", {
+					strong: (chunks) => <strong>{chunks}</strong>,
+				})}
+			</h2>
+			{Lists.map(
+				({ listKey, img, list, fullIMG, afterList, afterListImage }) => (
+					<div className="flex flex-col gap-6" key={listKey}>
+						<div className="flex items-center gap-4">
+							<NextImage
+								src={img}
+								alt={`Icon for ${listKey}`}
+								width={lisztIconSize}
+								height={lisztIconSize}
+							/>
+							<h3 className="font-normal">
+								{t.rich(`${listKey}Intro`, {
+									strong: (chunks) => <strong>{chunks}</strong>,
+								})}
 							</h3>
-							<p className="">
-								{t("recommendations.beforeEvent.personalPreparedness.item1")}
-							</p>
-							<h3 className="">
-								{t("recommendations.beforeEvent.personalPreparedness.title2")}
-							</h3>
-							<p className="">
-								{t.rich(
-									"recommendations.beforeEvent.personalPreparedness.item2",
-									{
+						</div>
+						<ul className="list-none space-y-2 lg:ps-12">
+							{Object.keys(list).map((key) => (
+								<li key={key} className="flex items-start gap-2">
+									<FontAwesomeIcon
+										icon={faCheck}
+										className={`flex-shrink-0 text-[18px]`}
+									/>
+									<span>
+										{t.rich(`${listKey}.${key}`, {
+											link: (chunks) => (
+												<Link
+													href={getLink(key, chunks, listKey).link}
+													target={getLink(key, chunks, listKey).target}
+													rel="noopener noreferrer"
+													className="text-text-link underline"
+												>
+													{chunks}
+												</Link>
+											),
+										})}
+									</span>
+								</li>
+							))}
+						</ul>
+						{afterList && (
+							<div className="flex flex-col gap-4 lg:flex-row">
+								<p
+									className={cn("lg:ps-12", afterListImage && "lg:max-w-[50%]")}
+								>
+									{t.rich(afterList, {
 										link1: (chunks) => (
 											<Link
-												href="https://www.bbk.bund.de/DE/Warnung-Vorsorge/Warn-App-NINA/warn-app-nina_node.html"
+												href="https://www.hochwasser-pass.info/"
 												target="_blank"
 												rel="noopener noreferrer"
+												className="text-text-link underline"
 											>
-												<Button variant="linkWithIcon" className="min-h-0">
-													{chunks}
-												</Button>
+												{chunks}
 											</Link>
 										),
 										link2: (chunks) => (
 											<Link
-												href="https://www.dwd.de/DE/service/dwd-apps/dwdapps_node.html"
+												href="https://www.fib-bund.de/inhalt/themen/hochwasser/"
 												target="_blank"
 												rel="noopener noreferrer"
+												className="text-text-link underline"
 											>
-												<Button variant="linkWithIcon" className="min-h-0">
-													{chunks}
-												</Button>
+												{chunks}
 											</Link>
 										),
-									},
+									})}
+								</p>
+								{afterListImage && (
+									<NextImage
+										src={afterListImage}
+										alt={`???`}
+										width={160}
+										height={160}
+									/>
 								)}
-							</p>
-							<h3 className="">
-								{t("recommendations.beforeEvent.personalPreparedness.title3")}
-							</h3>
-							<p className="">
-								{t("recommendations.beforeEvent.personalPreparedness.item3")}
-							</p>
-							<h3 className="">
-								{t("recommendations.beforeEvent.personalPreparedness.title4")}
-							</h3>
-							<p className="">
-								{t.rich(
-									"recommendations.beforeEvent.personalPreparedness.item4",
-									{
-										link: (chunks) => (
-											<Link
-												href="https://www.berlin.de/katastrophenschutz/warnung-und-information/anlaufstellen-fuer-die-bevoelkerung/"
-												target="_blank"
-												rel="noopener noreferrer"
-											>
-												<Button variant="linkWithIcon" className="min-h-0">
-													{chunks}
-												</Button>
-											</Link>
-										),
-									},
-								)}
-							</p>
-							<Link
-								href="https://www.bbk.bund.de/SharedDocs/Videos/DE/Warnung-Vorsorge/youtube-erklaerfilm-notgepaeck.html"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<Button variant="linkWithIcon" className="min-h-0">
-									{t(
-										"recommendations.beforeEvent.personalPreparedness.link3.title",
-									)}
-								</Button>
-							</Link>
-						</div>
-					}
-					slotB={
-						<div className="flex h-full flex-col gap-6">
-							<Image
-								className="-mx-5 w-screen max-w-none lg:-mx-0 lg:w-auto"
-								src="/A3_Schutzmaßnahmen_Schutzmaßnahmen.png"
-								alt={t(
-									"recommendations.beforeEvent.personalPreparedness.image4.alt",
-								)}
-								caption={t(
-									"recommendations.beforeEvent.personalPreparedness.image4.caption",
-								)}
-								copyright={t(
-									"recommendations.beforeEvent.personalPreparedness.image4.copyright",
-								)}
-							/>
-							<Image
-								className="-mx-5 w-screen max-w-none lg:-mx-0 lg:w-auto"
-								src="/A3_Schutzmaßnahmen_Schutzmaßnahmen_2.png"
-								alt={t(
-									"recommendations.beforeEvent.personalPreparedness.image5.alt",
-								)}
-								caption={t(
-									"recommendations.beforeEvent.personalPreparedness.image5.caption",
-								)}
-								copyright={t(
-									"recommendations.beforeEvent.personalPreparedness.image5.copyright",
-								)}
-							/>
-						</div>
-					}
-				/>
-			</section>
-			<section className="">
-				<TextBlock
-					desktopColSpans={{ col1: 3, col2: 2 }}
-					className="w-full gap-6"
-					slotA={
-						<div className="flex w-full flex-col gap-6">
-							<h3 className="">
-								{t("recommendations.beforeEvent.inBuilding.title")}
-							</h3>
-							<ul className="list-disc space-y-2 pl-6">
-								<li>{t("recommendations.beforeEvent.inBuilding.item1")}</li>
-								<li>{t("recommendations.beforeEvent.inBuilding.item2")}</li>
-								<li>{t("recommendations.beforeEvent.inBuilding.item3")}</li>
-								<li>{t("recommendations.beforeEvent.inBuilding.item4")}</li>
-								<li>{t("recommendations.beforeEvent.inBuilding.item5")}</li>
-								<li>{t("recommendations.beforeEvent.inBuilding.item6")}</li>
-								<li>{t("recommendations.beforeEvent.inBuilding.item7")}</li>
-								<li>{t("recommendations.beforeEvent.inBuilding.item8")}</li>
-							</ul>
-						</div>
-					}
-					slotB={
-						<Image
-							className="-mx-5 w-screen max-w-none lg:-mx-0 lg:w-auto"
-							src="/A3_Schutzmaßnahmen_Schutzmaßnahmen_3.png"
-							alt={t("recommendations.beforeEvent.inBuilding.image.alt")}
-							caption={t(
-								"recommendations.beforeEvent.inBuilding.image.caption",
-							)}
-							copyright={t(
-								"recommendations.beforeEvent.inBuilding.image.copyright",
-							)}
-						/>
-					}
-				/>
-			</section>
-			<section className="">
-				<TextBlock
-					desktopColSpans={{ col1: 2, col2: 3 }}
-					className="w-full gap-6"
-					reverseDesktopColumns={true}
-					slotA={
-						<div className="flex w-full flex-col gap-6">
-							<h3 className="">
-								{t("recommendations.beforeEvent.precautionBuilding.title")}
-							</h3>
-							<p className="">
-								{t("recommendations.beforeEvent.precautionBuilding.subtitle")}
-							</p>
-							<ul className="list-disc space-y-2 pl-6">
-								<li>
-									{t("recommendations.beforeEvent.precautionBuilding.item1")}
-								</li>
-								<li>
-									{t("recommendations.beforeEvent.precautionBuilding.item2")}
-								</li>
-								<li>
-									{t("recommendations.beforeEvent.precautionBuilding.item3")}
-								</li>
-								<li>
-									{t("recommendations.beforeEvent.precautionBuilding.item4")}
-								</li>
-								<li>
-									{t("recommendations.beforeEvent.precautionBuilding.item5")}
-								</li>
-								<li>
-									{t("recommendations.beforeEvent.precautionBuilding.item6")}
-								</li>
-								<li>
-									{t("recommendations.beforeEvent.precautionBuilding.item7")}
-								</li>
-								<li>
-									{t("recommendations.beforeEvent.precautionBuilding.item8")}
-								</li>
-
-								<li>
-									{t("recommendations.beforeEvent.precautionBuilding.item9")}
-								</li>
-								<li>
-									{t("recommendations.beforeEvent.precautionBuilding.item10")}
-								</li>
-							</ul>
-						</div>
-					}
-					slotB={
-						<Image
-							className="-mx-5 w-screen max-w-none lg:-mx-0 lg:w-auto"
-							src="/A3_Schutzmaßnahmen_Schutzmaßnahmen_4.png"
-							alt={t(
-								"recommendations.beforeEvent.precautionBuilding.image.alt",
-							)}
-							caption={t(
-								"recommendations.beforeEvent.precautionBuilding.image.caption",
-							)}
-							copyright={t(
-								"recommendations.beforeEvent.precautionBuilding.image.copyright",
-							)}
-						/>
-					}
-				/>
-			</section>
-			<section className="">
-				<TextBlock
-					desktopColSpans={{ col1: 3, col2: 2 }}
-					className="w-full gap-6"
-					slotA={
-						<div className="flex w-full flex-col gap-6">
-							<h3 className="">
-								{t("recommendations.beforeEvent.traffic.title")}
-							</h3>
-							<ul className="list-disc space-y-2 pl-6">
-								<li>{t("recommendations.beforeEvent.traffic.item1")}</li>
-								<li>{t("recommendations.beforeEvent.traffic.item2")}</li>
-								<li>{t("recommendations.beforeEvent.traffic.item3")}</li>
-								<li>{t("recommendations.beforeEvent.traffic.item4")}</li>
-								<li>{t("recommendations.beforeEvent.traffic.item5")}</li>
-							</ul>
-						</div>
-					}
-					slotB={
-						<Image
-							src="/A3_Schutzmaßnahmen_Schutzmaßnahmen_5.png"
-							alt={t("recommendations.beforeEvent.traffic.image.alt")}
-							className="w-full"
-							caption={t("recommendations.beforeEvent.traffic.image.caption")}
-							copyright={t(
-								"recommendations.beforeEvent.traffic.image.copyright",
-							)}
-						/>
-					}
-				/>
-			</section>
-			<section className="">
-				<TextBlock
-					desktopColSpans={{ col1: 2, col2: 3 }}
-					className="w-full gap-6"
-					reverseDesktopColumns={true}
-					slotA={
-						<div className="flex w-full flex-col gap-6">
-							<h3 className="">
-								{t("recommendations.beforeEvent.fluvialFlood.title")}
-							</h3>
-							<ul className="list-disc space-y-2 pl-6">
-								<li>{t("recommendations.beforeEvent.fluvialFlood.item1")}</li>
-								<li>{t("recommendations.beforeEvent.fluvialFlood.item2")}</li>
-								<li>{t("recommendations.beforeEvent.fluvialFlood.item3")}</li>
-							</ul>
-						</div>
-					}
-					slotB={
-						<Image
-							className="-mx-5 w-screen max-w-none lg:-mx-0 lg:w-auto"
-							src="/A3_Schutzmaßnahmen_Schutzmaßnahmen_6.jpg"
-							alt={t("recommendations.beforeEvent.fluvialFlood.image.alt")}
-							caption={t(
-								"recommendations.beforeEvent.fluvialFlood.image.caption",
-							)}
-							copyright={t(
-								"recommendations.beforeEvent.fluvialFlood.image.copyright",
-							)}
-						/>
-					}
-				/>
-			</section>
-		</div>
+							</div>
+						)}
+						{fullIMG && (
+							<>
+								<div className="flex justify-center">
+									<Image
+										className="hidden w-[75%] lg:block"
+										src={fullIMG}
+										alt={t(`${listKey}Image.alt`)}
+										caption={t(`${listKey}Image.caption`)}
+										copyright={t(`${listKey}Image.copyright`)}
+										withZoomBox
+									/>
+								</div>
+								<Image
+									className="w-[calc(100%+3rem)] -translate-x-[1.5rem] lg:hidden"
+									src={fullIMG}
+									alt={t(`${listKey}Image.alt`)}
+									caption={t(`${listKey}Image.caption`)}
+									copyright={t(`${listKey}Image.copyright`)}
+									withZoomBox
+								/>
+							</>
+						)}
+					</div>
+				),
+			)}
+		</section>
 	);
 };
 
--- src/components/Handlungsempfehlungen/Emergency.tsx
+++ src/components/Handlungsempfehlungen/Emergency.tsx
@@ -1,27 +1,183 @@
-import React from "react";
-// import { useTranslations } from "next-intl";
-import TextBlock from "../TextBlock";
-/* import { Button, Image } from "berlin-ui-library";
-import Link from "next/link"; */
+import React, { ReactNode } from "react";
+import { useMessages, useTranslations } from "next-intl";
+import Link from "next/link";
+import NextImage from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { Image } from "berlin-ui-library";
+
+type TocMap = Record<string, string>;
+const lisztIconSize = 32;
 
 const Emergency: React.FC = () => {
-	// const t = useTranslations();
+	const t = useTranslations("recommendations.emergency");
+	const content = useMessages() as {
+		recommendations: {
+			emergency: {
+				list1: TocMap;
+				list2: TocMap;
+				list3: TocMap;
+				list4: TocMap;
+				list5: TocMap;
+			};
+		};
+	};
+	const list1 = content.recommendations.emergency.list1;
+	const list2 = content.recommendations.emergency.list2;
+	const list3 = content.recommendations.emergency.list3;
+	const list4 = content.recommendations.emergency.list4;
+	const list5 = content.recommendations.emergency.list5;
+
+	const getLink = (key: string, chunks: ReactNode) => {
+		let text = "";
+		if (typeof chunks === "string") {
+			text = chunks;
+		} else if (Array.isArray(chunks)) {
+			text = chunks.join("");
+		}
+
+		if (key === "entry1") {
+			return {
+				target: "_blank",
+				link: text.includes("NINA")
+					? "https://www.bbk.bund.de/DE/Warnung-Vorsorge/Warn-App-NINA/warn-app-nina_node.html"
+					: "https://www.dwd.de/DE/service/dwd-apps/dwdapps_node.html",
+			};
+		}
+		if (key === "entry2") {
+			return {
+				target: "_blank",
+				link: "https://www.berlin.de/katastrophenschutz/warnung-und-information/sirenen/artikel.1578804.php#headline_1_39",
+			};
+		}
+		if (key === "entry3") {
+			return {
+				target: "_self",
+				link: "/",
+			};
+		}
+		return {
+			target: "_self",
+			link: "#",
+		};
+	};
+
+	const Lists = [
+		{
+			listKey: "list1",
+			img: "/HandlungsempfehlungIcons/Icon_Warn.png",
+			list: list1,
+		},
+		{
+			listKey: "list2",
+			img: "/HandlungsempfehlungIcons/icon_EmgerncyPackage.png",
+			list: list2,
+		},
+		{
+			listKey: "list3",
+			img: "/HandlungsempfehlungIcons/icon_HelpingOthers.png",
+			list: list3,
+		},
+		{
+			listKey: "list4",
+			img: "/HandlungsempfehlungIcons/Icon_Auto.png",
+			list: list4,
+			fullIMG: "/Verkehr-Autoumpacken.png",
+		},
+		{
+			listKey: "list5",
+			img: "/HandlungsempfehlungIcons/icon_Nassvorsorge.png",
+			list: list5,
+		},
+	];
 	return (
-		<div className="flex flex-col gap-12">
-			<section className="">
-				<TextBlock
-					desktopColSpans={{ col1: 2, col2: 3 }}
-					className="w-full gap-6"
-					reverseDesktopColumns={true}
-					slotA={
-						<div className="flex w-full flex-col gap-6">
-							<h3 className="">NOTFALL!</h3>
-						</div>
-					}
-					slotB={<></>}
-				/>
-			</section>
-		</div>
+		<section className="mb-12 flex flex-col gap-12">
+			<h2 className="font-normal">
+				{t.rich("intro", {
+					strong: (chunks) => <strong>{chunks}</strong>,
+				})}
+			</h2>
+			<div className="bg-message-error p-6">
+				<p className="whitespace-pre-line">
+					{t.rich("warning1", {
+						strong: (chunks) => <strong>{chunks}</strong>,
+					})}
+				</p>
+			</div>
+			{Lists.map(({ listKey, img, list, fullIMG }) => (
+				<div className="flex flex-col gap-6" key={listKey}>
+					<div className="flex items-center gap-4">
+						<NextImage
+							src={img}
+							alt={`Icon for ${listKey}`}
+							width={lisztIconSize}
+							height={lisztIconSize}
+						/>
+						<h3 className="font-normal">
+							{t.rich(`${listKey}Intro`, {
+								strong: (chunks) => <strong>{chunks}</strong>,
+								underline: (chunks) => (
+									<span className="underline">{chunks}</span>
+								),
+							})}
+						</h3>
+					</div>
+					<ul className={"list-none space-y-2 lg:ps-12"}>
+						{Object.keys(list).map((key) => (
+							<li key={key} className="flex items-start gap-2">
+								<FontAwesomeIcon
+									icon={faCheck}
+									className={`flex-shrink-0 text-[18px]`}
+								/>
+								<span className="whitespace-pre-line">
+									{t.rich(`${listKey}.${key}`, {
+										link: (chunks) => (
+											<Link
+												href={getLink(key, chunks).link}
+												target={getLink(key, chunks).target}
+												rel="noopener noreferrer"
+												className="text-text-link underline"
+											>
+												{chunks}
+											</Link>
+										),
+									})}
+								</span>
+							</li>
+						))}
+					</ul>
+					{fullIMG && (
+						<>
+							<div className="flex justify-center">
+								<Image
+									className="hidden w-[75%] lg:block"
+									src={fullIMG}
+									alt={t(`${listKey}Image.alt`)}
+									caption={t(`${listKey}Image.caption`)}
+									copyright={t(`${listKey}Image.copyright`)}
+									withZoomBox
+								/>
+							</div>
+							<Image
+								className="w-[calc(100%+3rem)] -translate-x-[1.5rem] lg:hidden"
+								src={fullIMG}
+								alt={t(`${listKey}Image.alt`)}
+								caption={t(`${listKey}Image.caption`)}
+								copyright={t(`${listKey}Image.copyright`)}
+								withZoomBox
+							/>
+						</>
+					)}
+				</div>
+			))}
+			<div className="bg-message-error p-6">
+				<p className="whitespace-pre-line">
+					{t.rich("warning2", {
+						strong: (chunks) => <strong>{chunks}</strong>,
+					})}
+				</p>
+			</div>
+		</section>
 	);
 };
 
