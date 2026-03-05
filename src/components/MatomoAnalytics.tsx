c59450bdc1b6786bcd1894f3541e87f8fb27f5e4
Luise Brandenburger <luise.brandenburger@ts.berlin>
2026-03-05 16:58:16 +0100

    feat: test matomo tracking

--- src/components/MatomoAnalytics.tsx
+++ src/components/MatomoAnalytics.tsx
@@ -11,20 +11,31 @@ export default function MatomoAnalytics() {
 	const pathname = usePathname();
 	const searchParams = useSearchParams();
 
+	// useEffect(() => {
+	// 	if (!MATOMO_URL || !MATOMO_SITE_ID) return;
+
+	// 	(window as any)._paq = (window as any)._paq || [];
+	// 	(window as any)._paq.push(["disableCookies"]);
+
+	// 	trackAppRouter({
+	// 		url: MATOMO_URL,
+	// 		siteId: MATOMO_SITE_ID,
+	// 		pathname,
+	// 		searchParams,
+	// 		disableCookies: true,
+	// 	});
+	// }, [pathname, searchParams]);
+
 	useEffect(() => {
 		if (!MATOMO_URL || !MATOMO_SITE_ID) return;
 
-		(window as any)._paq = (window as any)._paq || [];
-		(window as any)._paq.push(["disableCookies"]);
-
-		trackAppRouter({
-			url: MATOMO_URL,
-			siteId: MATOMO_SITE_ID,
-			pathname,
-			searchParams,
-			disableCookies: true,
-		});
-	}, [pathname, searchParams]);
+		const _paq = (window as any)._paq || [];
+		(window as any)._paq = _paq;
+		_paq.push(["disableCookies"]);
+		_paq.push(["setTrackerUrl", `${MATOMO_URL}/matomo.php`]);
+		_paq.push(["setSiteId", MATOMO_SITE_ID]);
+		_paq.push(["trackPageView"]);
+	}, []);
 
 	return null;
 }
