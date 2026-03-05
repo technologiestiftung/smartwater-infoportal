17b064ade70230188045efcd3100057f018fc66f
Luise Brandenburger <luise.brandenburger@ts.berlin>
2026-03-05 13:43:06 +0100

    fix: cookies settings

--- src/components/MatomoAnalytics.tsx
+++ src/components/MatomoAnalytics.tsx
@@ -4,19 +4,22 @@ import { trackAppRouter } from "@socialgouv/matomo-next";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
-const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL!;
-const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID!;
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
 
-export function MatomoAnalytics() {
+export default function MatomoAnalytics() {
 	const pathname = usePathname();
 	const searchParams = useSearchParams();
 
 	useEffect(() => {
+		if (!MATOMO_URL || !MATOMO_SITE_ID) return;
+
 		trackAppRouter({
 			url: MATOMO_URL,
 			siteId: MATOMO_SITE_ID,
 			pathname,
 			searchParams,
+			disableCookies: true,
 		});
 	}, [pathname, searchParams]);
 
